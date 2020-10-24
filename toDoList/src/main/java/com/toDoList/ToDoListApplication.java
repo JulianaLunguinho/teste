d17621c11f101740d8382d6e774d2c422b0abf9e
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-10-24 15:20:29 -0300

    mais um

--- toDoList/src/main/java/com/toDoList/ToDoListApplication.java
+++ toDoList/src/main/java/com/toDoList/ToDoListApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ToDoListApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToDoListApplication.class, args);
-	}
-	// teste
-
-	// juju commitou aqui
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListApplication.class, args);
+    }
+    /*
+    teste
+    juju commitou aqui
+    --coloque o prox aqui
+    */
 }
